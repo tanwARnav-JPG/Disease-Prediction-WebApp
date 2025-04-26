7cc4f955ff2329e77de1791609fd178373d51fe1
tanwARnav-JPG <arnavtanwar18@gmail.com>
2025-04-26 22:53:32 +0530

    Update multiple disease pred1.py

--- multiple disease pred1.py
+++ multiple disease pred1.py
@@ -2,7 +2,7 @@
 import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
-import seaborn as sns
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
