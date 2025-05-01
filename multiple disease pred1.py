7dc59fe2510a9059522ca09df527d506d0ded46b
tanwARnav-JPG <arnavtanwar18@gmail.com>
2025-05-02 00:26:29 +0530

    Update multiple disease pred1.py

--- multiple disease pred1.py
+++ multiple disease pred1.py
@@ -55,7 +55,7 @@ with st.sidebar:
 
 if selected == 'CHOOSE AN OPTION':
     
-    st.success("👨🏻‍💻 WELCOME TO THIS ML-BASED MENU-DRIVEN HEALTH PREDICTION WEB APP.")
+    st.success("👨🏻‍💻 WELCOME TO A ML-BASED MENU-DRIVEN HEALTH PREDICTION WEB APP.")
     st.info("🖥 Designed with a user-friendly interface and backed by real medical datasets and models, this system uses advanced ML algorithms to predict various health conditions based on user input including: Diabetes, Heart Disease, Parkinson's Disease, Breast Cancer, Lung Cancer.")
     st.error("🚀 Choose a prediction model from the sidebar to get started.")
     st.warning("⚠️ This tool is for educational use and not a substitute for professional medical advice!")
